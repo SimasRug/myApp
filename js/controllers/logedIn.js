2b861a815eddd904494f4b2a4b53abe73fbf9ac7
SimasRug <simonas.rugevicius@gmail.com>
2017-02-05 12:28:16 +0100

    User is able to write data to database

--- js/controllers/logedIn.js
+++ js/controllers/logedIn.js
@@ -1,7 +1,33 @@
 (function () {
 
-myApp.controller('LogedInController', ['$scope', function($scope) {
-$scope.success = "Success!!";
+myApp.controller('LogedInController',
+  ['$scope', '$firebaseAuth', '$firebaseArray',
+  function($scope, $firebaseAuth, $firebaseArray){
+
+    var ref = firebase.database().ref(); // refrence to the database
+    var auth = $firebaseAuth(); // used for authentication
+
+
+
+    auth.$onAuthStateChanged(function(authUser) {
+      if(authUser) {
+        var meetingsRef = ref.child('users').child(authUser.uid).child('meetings');
+        var meetingsInfo = $firebaseArray(meetingsRef);
+        console.log(meetingsInfo);
+        console.log(authUser.uid);
+        $scope.addMeeting = function() {
+          console.log('a');
+          meetingsInfo.$add({
+            name: $scope.meetingName,
+            date: firebase.database.ServerValue.TIMESTAMP
+          }).then(function(){
+             $scope.meetingName = '';
+          });
+        };
+      }
+    });
+
+
 }]);
 
 })();
